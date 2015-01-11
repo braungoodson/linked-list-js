8601c58f25ee48ee2543bf97a105a0445d03389e
Ian Macalinao <me@ian.pw>
2015-01-10 20:23:53 -0600

    Add iterator

--- index.js
+++ index.js
@@ -16,4 +16,24 @@ function LinkedList(arr) {
   }
 }
 
+/**
+ * LinkedList iterator
+ */
+LinkedList.prototype.iterator = function() {
+  var list = this;
+  return {
+    next: function() {
+      if (this.node && !this.node.next) {
+        throw new Error('At tail of list!');
+      }
+      this.node = this.node ? this.node.next : list.head;
+      return this.node.value;
+    },
+    hasNext: function() {
+      if (!this.node) return list.head;
+      return this.node.next;
+    }
+  };
+};
+
 module.exports = LinkedList;
--- test.js
+++ test.js
@@ -5,15 +5,21 @@ describe('linked-list', function() {
   it('should create a linked list', function() {
     var list = new LinkedList([1, 2, 3, 4, 5, 6, 7, 8]);
 
-    this.node = null;
-
-    function next() {
-      this.node = this.node ? this.node.next : list.head;
-      return this.node.value;
-    }
+    var it = list.iterator();
 
     for (var i = 1; i <= 8; i++) {
-      expect(next()).to.equal(i);
+      expect(it.next()).to.equal(i);
     }
+  });
+
+  it('should error if iteration goes over', function() {
+    var list = new LinkedList([1, 2, 3]);
+    var it = list.iterator();
+    it.next();
+    it.next();
+    it.next();
+    expect(function() {
+      it.next()
+    }).to.throw(/At tail/);
   });
 });
